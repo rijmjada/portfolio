ea28fdc1491b2bc3eb19cd5fbcca3195a52753bb
Diego <rijmjada@gmail.com>
2023-05-13 23:19:18 -0300

    first

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let typingText = new Typed(".change-text", {
-    strings: ["Estudiante", "Coder" ,"Progammer", "Full-Stack", "Developer"],
+    strings: ["Estudiante", "Developer", "Full-Stack", "Coder", "Progammer",],
     loop: true,
     typeSpeed: 85,
     backSpeed: 45,
