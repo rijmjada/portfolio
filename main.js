5a4c366dbe36096024e6bc0ab6b37a69345fa56d
Diego <rijmjada@gmail.com>
2023-10-28 19:58:17 -0300

    scroll btn

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ let typingText = new Typed(".change-text", {
 });
 
 
+// Función para mostrar u ocultar el botón según el desplazamiento de la página
+function toggleScrollButton() {
+    const scrollButton = document.querySelector('#scrollButton');
+    if (document.documentElement.scrollTop > 200) {
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
+    }
+}
+
+// Función para hacer scroll hasta arriba
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Agregar el evento de scroll al botón
+document.addEventListener('scroll', toggleScrollButton);
+
+// Agregar el evento de click al botón
+const scrollButton = document.querySelector('#scrollButton');
+scrollButton.addEventListener('click', scrollToTop);
