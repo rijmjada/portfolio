2e3bab35e9f9d17faf44ad1f105f24b1e1bc1d0b
Diego <rijmjada@gmail.com>
2024-02-18 20:36:28 -0300

    colmena js

--- main.js
+++ main.js
@@ -31,3 +31,32 @@ document.addEventListener('scroll', toggleScrollButton);
 // Agregar el evento de click al botón
 const scrollButton = document.querySelector('#scrollButton');
 scrollButton.addEventListener('click', scrollToTop);
+
+
+
+
+
+
+let elementosHex = document.querySelectorAll('.hex');
+let index = 0;
+let subIndex = 0;
+
+function cambiarOpacidad() {
+    if (index < elementosHex.length) {
+        if (subIndex === 0) {
+            elementosHex[index].classList.add('opacidad-1');
+        } else {
+            elementosHex[index].classList.remove('opacidad-1');
+            index++;
+        }
+        subIndex = 1 - subIndex;
+    } else {
+        // Reiniciar el índice cuando se procesan todos los elementos
+        index = 0;
+    }
+}
+
+// Configurar un intervalo para cambiar la opacidad cada 500 milisegundos
+let intervalId = setInterval(cambiarOpacidad, 300);
+
+
